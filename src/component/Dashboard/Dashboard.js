6697add96970bc777cad8a7bb1592b22268551f4
Alfred Pithu <alfredpithu@gmail.com>
2022-04-05 07:24:18 +0600

    chart completed and home revisited

--- src/component/Dashboard/Dashboard.js
+++ src/component/Dashboard/Dashboard.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './Dashboard.css'
-import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, AreaChart, CartesianGrid, Area } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer, AreaChart, CartesianGrid, Area, BarChart, Bar, PieChart, Pie } from 'recharts';
 
 
 const Dashboard = () => {
@@ -15,13 +15,13 @@ const Dashboard = () => {
 
     console.log(data);
     return (
-        <div className='container'>
+        <div className='container '>
             {/* <h2>Dashboard</h2> */}
 
-            <div className='d-flex'>
+            <div className='row g-5'>
 
-                <div className='border border-1 w-50 '>
-                    <h6 className='text-center'>Month-wise sales</h6>
+                <div className='  col-lg-6 col-12 '>
+                    <h6 className='text-center  text-primary'>Month-wise sales</h6>
 
                     <LineChart className='mx-auto' width={300} height={200} data={data}>
 
@@ -34,8 +34,8 @@ const Dashboard = () => {
                 </div>
 
                 {/* AreaChart  */}
-                <div className='border border-1 w-50 '>
-                    <h6 className='text-center'>Investment VS Revenue</h6>
+                <div className=' col-lg-6  col-12 '>
+                    <h6 className='text-center text-primary'>Investment VS Revenue</h6>
                     <AreaChart
                         className='mx-auto'
                         width={300}
@@ -55,6 +55,41 @@ const Dashboard = () => {
                         <Area type="monotone" dataKey="investment" stackId="1" stroke="#8884d8" fill="#8884d8" />
                         <Area type="monotone" dataKey="revenue" stackId="1" stroke="#82ca9d" fill="#82ca9d" />
                     </AreaChart>
+                </div>
+
+                {/* BarChart */}
+                <div className='  col-lg-6  col-12 '>
+                    <h6 className='text-center text-primary'>Investment VS Revenue</h6>
+                    <BarChart
+                        className='mx-auto'
+                        width={300}
+                        height={200}
+                        data={data}
+                        margin={{
+                            top: 20,
+                            right: 30,
+                            left: 20,
+                            bottom: 5,
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Bar dataKey="investment" stackId="a" fill="#8884d8" />
+                        <Bar dataKey="revenue" stackId="a" fill="#82ca9d" />
+                    </BarChart>
+                </div>
+
+                {/* PieChart */}
+                <div className='  col-lg-6  col-12 '>
+                    <h6 className='text-center text-primary'>Investment VS Revenue</h6>
+                    <PieChart className='mx-auto' width={300} height={300}>
+                        <Pie data={data} dataKey="investment" cx='50%' cy='50%' outerRadius={60} fill="#8884d8" />
+                        <Pie data={data} dataKey="revenue" cx='50%' cy='50%' innerRadius={70} outerRadius={90} fill="#82ca9d" label />
+                        <Tooltip></Tooltip>
+                    </PieChart>
                 </div>
 
 
--- src/component/Home/Home.js
+++ src/component/Home/Home.js
@@ -12,24 +12,24 @@ const Home = () => {
     console.log(modifiedData);
     return (
         <div>
-            <div className='home-container container mt-5  h-100 ' >
-                <div className='w-75 mx-auto'>
+            <div className='home-container container mt-5 mb-3 row ' >
+
+                <div className='  mx-auto col-12 col-lg-7 d-flex flex-column align-items-center justify-content-center '>
                     <h1 className='fw-bolder text-primary text-center mb-4'>Your New Car</h1>
                     <p className='text-start'>
                         Choosing your new car is always a tough choice we understand. Luckily you are at the right place and we got you covered. Check the reviews of this model to get the answers to your all questions.
                     </p>
                 </div>
 
-
-                <div className=''>
-                    <img className='img-fluid ' src={car} alt="" />
+                <div className='col-12 col-lg-5 d-flex align-items-center'>
+                    <img className='img-fluid border rounded-2 ' src={car} alt="" />
                 </div>
             </div>
 
-            <div className='mt-5'>
+            <div className=''>
                 <h2 className='text-center'>Customer Reviews</h2>
 
-                <div className=' container mx-auto row row-cols-1 row-cols-lg-3 g-3 my-5'>
+                <div className=' container mx-auto row row-cols-1 row-cols-lg-3 g-3 mt-4 mb-3'>
                     {
                         modifiedData.map(review => <Card key={review.id} review={review}></Card>)
                     }
